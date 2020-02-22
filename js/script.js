94d96d892fa80dc46b12a124b9b2ee191bb18953
Adnan Kibe <adnangitonga@gmail.com>
2020-02-22 17:51:43 +0300

    Added more content to what we do segment and hide it.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-document.getElementById("marker").addEventListener("click", function(event){
-  event.preventDefault()
+
+$(document).ready(function(){
+  $('#img1').on('click',function(){
+    $('#design_hide').show();
+    $('#img1').hide();
+  });
+
+    event.preventDefault()
 });
